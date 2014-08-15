d58bc978760da7a7babe97f4fe68360da3d1de9f
per <per@karlssonper.com>
2014-08-15 09:18:59 +0200

    build script backup
    Former-commit-id: 7dfab1d81130165a0e6bd0a7c7761cc35d1faa29

--- build_3rdparty.py
+++ build_3rdparty.py
@@ -2,7 +2,20 @@
 import sys
 import os
 import shutil
+import glob
+import re
 
+# order matters
+_3RDPARTYLIBS = [
+    "zlib", 
+    "openexr", 
+    "boost_python", 
+    "boost_numpy",
+    "glew",
+    "glfw", 
+    "png", 
+    "cimg"
+]
 _TARGET = "install" if sys.platform != "win32" else "INSTALL"
 _CWD = os.getcwd()
 
@@ -11,31 +24,56 @@ def _cmd(command):
     if os.system(command):
         sys.exit(1)
 
+def _lib_exists(lib, install_prefix):
+    prefix, suffixA, suffixB = "", "", ""
+    if os.uname()[0] == "Linux":
+        prefix, suffixA, suffixB = "lib", "so", "a"
+    elif os.uname()[0] == "Darwin":
+        prefix, suffixA, suffixB = "lib", "dylib", "a"
+    elif os.uname()[0] == "Win32":
+        prefix, suffixA, suffixB = "", "dll", "lib"
+
+    files = glob.glob(os.path.realpath(install_prefix) +"/lib/*."+suffixA) + \
+            glob.glob(os.path.realpath(install_prefix) +"/lib/*."+suffixB)
+    
+    found = any(x.count(prefix+lib) > 0 and \
+                (x.endswith(suffixA) or x.endswith(suffixB)) for x in files)
+    if found:
+        print "Found " + prefix + lib + " in " + install_prefix
+    return found
+
 def init_update_submodule(name):
     _cmd("git submodule init %s" % name)
     _cmd("git submodule update %s" % name)
 
 def build(name, install_prefix, cmake_extraargs = ""):
-    if not os.path.exists("3rdparty/build/%s" % name):
-        os.makedirs("3rdparty/build/%s" % name)
-    os.chdir("3rdparty/build/%s" % name)
+    if not os.path.exists(_CWD + "/3rdparty/build/%s" % name):
+        os.makedirs(_CWD + "/3rdparty/build/%s" % name)
+    os.chdir(_CWD + "/3rdparty/build/%s" % name)
     _cmd("cmake %s/3rdparty/%s %s -DCMAKE_INSTALL_PREFIX=%s" \
          % (_CWD, name, cmake_extraargs, install_prefix))
+    
     # Remove install cache (will install to old CMAKE_INSTALL_PREFIX if not
-    _cmd("cmake %s ." % ("-UINSTALL_BIN_DIR "
-                         "-UINSTALL_INC_DIR "
-                         "-UINSTALL_LIB_DIR "
-                         "-UINSTALL_MAN_DIR "
-                         "-UINSTALL_PKGCONFIG_DIR"))
+    _cmd("cmake %s ." % (" ".join(["-UINSTALL_%s_DIR" % s \
+                                   for s in ["INC","LIB","MAN","PKGCONFIG"]])))
     _cmd("cmake --build . --config Release --target %s -- -j8 " % _TARGET)
 
 def build_zlib(install_prefix):
+    if _lib_exists("z", install_prefix):
+        return
+
     print "Building ZLIB..."
+
     init_update_submodule("3rdparty/zlib")
     build("zlib",install_prefix)
 
 def build_openexr(install_prefix):
+    libs = ["Half", "Iex", "IexMath", "IlmImf", "IlmThread", "Imath"]
+    if all(_lib_exists(lib, install_prefix) for lib in libs):
+        return
+
     print "Building OpenEXR..."
+
     init_update_submodule("3rdparty/openexr")
     build("openexr/IlmBase", install_prefix)
     os.chdir(_CWD)
@@ -50,37 +88,62 @@ def build_openexr(install_prefix):
                 txt.replace(remove_str, "#" + remove_str))
   
     build("openexr/OpenEXR", install_prefix, 
-          "-DZLIB_ROOT=%s " % _INSTALL_PREFIX + \
-          "-DILMBASE_PACKAGE_PREFIX=%s" % _INSTALL_PREFIX)
+          "-DZLIB_ROOT=%s " % install_prefix + \
+          "-DILMBASE_PACKAGE_PREFIX=%s" % install_prefix)
 
 def build_boost_python(install_prefix):
+    if _lib_exists("boost_python", install_prefix):
+        return
+
     print "Building Boost Python..."
     init_update_submodule("3rdparty/boost")
     os.chdir("3rdparty/boost")
-    init_update_submodule("libs/python")
-    init_update_submodule("libs/wave")
+    # Only download necessary boost modules (should save some time)
+    libs = [ "array", "assert", "bind", "concept_check", "config", "container",
+             "conversion", "core", "detail", "foreach", "function", 
+             "functional", "graph", "integer", "iterator", "lexical_cast",
+             "math", "move", "mpl", "multi_index", "numeric/conversion", 
+             "optional", "parameter", "predef", "preprocessor", "property_map",
+             "python", "range", "serialization", "smart_ptr", "static_assert", 
+             "throw_exception", "tuple", "type_traits", "typeof", "unordered",
+             "utility", "wave" ]
+    for lib in libs:
+       init_update_submodule("libs/" + lib)
     init_update_submodule("tools/build")
     init_update_submodule("tools/inspect")
     _cmd("./bootstrap.sh")
+    _cmd("./b2 headers")
     _cmd("./b2 install --with-python --build-dir=%s --prefix=%s"\
               % (_CWD + "/3rdparty/build", install_prefix))
 
 def build_boost_numpy(install_prefix):
+    if _lib_exists("boost_numpy", install_prefix):
+        return
+
     print "Building Boost Numpy..."
     init_update_submodule("3rdparty/Boost.NumPy")
-    build("Boost.NumPy",install_prefix)
+    build("Boost.NumPy",install_prefix, "-DBOOST_ROOT=%s" % install_prefix)
 
 def build_glew(install_prefix):
+    if _lib_exists("glew", install_prefix):
+        return
+    
     print "Building GLEW..."
     init_update_submodule("3rdparty/glew")
-    build("glew", install_prefix)
+    build("glew", install_prefix, "-DONLY_LIBS=TRUE")
 
 def build_glfw(install_prefix):
+    if _lib_exists("glfw3", install_prefix):
+        return
+
     print "Building GLFW..."
     init_update_submodule("3rdparty/glfw")
     build("glfw", install_prefix)
 
 def build_png(install_prefix):
+    if _lib_exists("png", install_prefix):
+        return
+
     print "Building PNG..."
     init_update_submodule("3rdparty/libpng")
     build("libpng", install_prefix)
@@ -88,29 +151,23 @@ def build_png(install_prefix):
 def build_cimg(install_prefix):
     print "Building CImg..."
     init_update_submodule("3rdparty/CImg")
-    if not os.path.exists(_INSTALL_PREFIX + "/include"):
-        os.mkdir(_INSTALL_PREFIX + "/include")
+    if not os.path.exists(install_prefix + "/include"):
+        os.mkdir(install_prefix + "/include")
     # Header only library
     shutil.copy("3rdparty/CImg/CImg.h", install_prefix + "/include/CImg.h")
 
 def build_3rdparty(args, install_prefix):
     deps = [arg.lower() for arg in args]
-    # order matters
-    thirdpartylibs = ["zlib", 
-                      "openexr", 
-                      "boost_python", 
-                      "boost_numpy",
-                      "glew",
-                      "glfw", 
-                      "png", 
-                      "cimg"]
-    for lib in thirdpartylibs:
+    for lib in _3RDPARTYLIBS:
         if lib in deps:
             os.chdir(_CWD)
             exec("build_%s('%s')" % (lib, install_prefix))
 
 if __name__ == "__main__":
-    if sys.argv[1] == "--install-prefix":
+    if len(sys.argv) > 1 and sys.argv[1] == "--install-prefix":
         build_3rdparty(sys.argv[3:], os.path.realpath(sys.argv[2]))
     else:
-        print "error: usage is \n build_3rdparty --install-prefix path deps"
+        print "usage:\nbuild_3rdparty --install-prefix path lib1 lib2 lib3\n"
+        print "available third party libs:"
+        for lib in _3RDPARTYLIBS:
+            print "  " + lib
