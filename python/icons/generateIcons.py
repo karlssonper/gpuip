97c683da1df2842dc8d14fa2e974899205fed0f5
Per <per@enzo.itn.liu.se>
2014-08-21 13:53:01 +0200

    use numpy to switch r and b instead of imagemagick commandline

--- python/icons/generateIcons.py
+++ python/icons/generateIcons.py
@@ -13,34 +13,22 @@ except:
     open(sys.argv[1], "w").write(txt)
     sys.exit(0)
 
-icons = {}
-for i in glob.glob("*.png"):
-    icons[i[:i.find(".")]] = i
-
-out = """
-from PySide import QtGui
-data = {}
-width = {}
-height = {}
+out = """from PySide import QtGui
+data, width, height = {}, {}, {}
 def get(name):
     image = QtGui.QImage(data[name], width[name], height[name], 
                          QtGui.QImage.Format_ARGB32)
     return QtGui.QIcon(QtGui.QPixmap.fromImage(image))
 """
 
-for i in icons:
-    im = Image.open(icons[i])
-    im = im.convert("RGBA")
-    data = numpy.asarray(im)
-    data_flip = numpy.zeros(data.shape, dtype = data.dtype)
+for i in glob.glob("*.png"):
+    name = i[:i.find(".")]
+    data = numpy.asarray(Image.open(i).convert("RGBA"))
+    data_flip = numpy.array(data, copy=True)
     data_flip[:,:,0] = data[:,:,2]
-    data_flip[:,:,1] = data[:,:,1]
     data_flip[:,:,2] = data[:,:,0]
-    data_flip[:,:,3] = data[:,:,3]
-    im = Image.fromarray(data_flip.transpose())
-    w,h = im.size[0], im.size[1]
-    out += "data['" + i + "'] = " + repr(im.tobytes()) + " \n"
-    out += "width['" + i + "'] = " + str(w) + "\n"
-    out += "height['" + i + "'] = " + str(h) + "\n"
+    out += "data['" + name + "'] = " + repr(data_flip.tostring()) + " \n"
+    out += "width['" + name + "'] = " + str(data_flip.shape[0]) + "\n"
+    out += "height['" + name + "'] = " + str(data_flip.shape[1]) + "\n"
 
 open(sys.argv[1], "w").write(out)
